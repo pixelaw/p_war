9968f2042994690f879de354ea11bf5cca1a4d4b
0xshora <112358132134.fibon@gmail.com>
2024-07-05 20:29:45 +0200

    chore: update scripts

--- scripts/extend_end_game.sh
+++ scripts/extend_end_game.sh
@@ -19,6 +19,16 @@ function create_proposal() {
   sleep 0.3
 }
 
+function activate_proposal() {
+  sozo \
+  execute \
+  0x481aadb9eab76d625be9d0ab0af0155c9f162fd3af1113abd0d0308ddb9346e \
+  activate_proposal \
+  -c $1
+
+  sleep 0.3
+}
+
 # create game
 interact 1,1
 
@@ -27,6 +37,9 @@ interact 1,1
 
 # fn create_proposal(game_id: usize, proposal_type: ProposalType, target_args_1: u32, target_args_2: u32) -> usize;
 # addNewColor -> 2
-create_proposal 1,2,0,0 # 2->reset to white
-create_proposal 1,1,1,0 # 1-> add a color
-create_proposal 1,3,3,0
+create_proposal 1,2,255,0 # 2->reset to white
+create_proposal 1,1,3272281087,0 # 1-> add a color
+create_proposal 1,3,100000,0 # 3-> extend end game
+create_proposal 1,4,20,20 # 4-> expand area
+
+# activate_proposal 1,13 # activate a proposal
