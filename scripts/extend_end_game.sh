e80c47172071610d44486d44b9601726bea9ff93
0xshora <112358132134.fibon@gmail.com>
2024-07-05 23:00:15 +0200

    chore

--- scripts/extend_end_game.sh
+++ scripts/extend_end_game.sh
@@ -37,9 +37,10 @@ interact 1,1
 
 # fn create_proposal(game_id: usize, proposal_type: ProposalType, target_args_1: u32, target_args_2: u32) -> usize;
 # addNewColor -> 2
-create_proposal 1,2,255,0 # 2->reset to white
 create_proposal 1,1,3272281087,0 # 1-> add a color
-create_proposal 1,3,100000,0 # 3-> extend end game
+create_proposal 1,2,255,0 # 2->reset to white
+create_proposal 1,3,86410,0 # 3-> extend end game
 create_proposal 1,4,20,20 # 4-> expand area
 
-# activate_proposal 1,13 # activate a proposal
+# activate_proposal 1,1 # activate a proposal
+# activate_proposal 1,3 # activate a proposal
